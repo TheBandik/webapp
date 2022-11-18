4d28a3af51275710e60d0e1084b830643ba7068e
TheBandik <shnr.arky@gmail.com>
2022-11-18 15:03:14 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
 
-let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+let tg = window.Telegram.WebApp;
 
-tg.expand(); //расширяем на все окно  
-
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
+tg.expand();
 
 let btn = document.getElementById("btn"); //получаем кнопку скрыть/показать 
 
-btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-    if (tg.MainButton.isVisible){ //если кнопка показана 
-        tg.MainButton.hide() //скрываем кнопку 
-    }
-    else{ //иначе
-        tg.MainButton.show() //показываем 
-    }
-});
+// btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+//     if (tg.MainButton.isVisible){ //если кнопка показана 
+//         tg.MainButton.hide() //скрываем кнопку 
+//     }
+//     else{ //иначе
+//         tg.MainButton.show() //показываем 
+//     }
+// });
 
 let btnED = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
 btnED.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
@@ -33,26 +27,5 @@ btnED.addEventListener('click', function(){ //вешаем событие на �
 });
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    tg.sendData("some string that we need to send"); 
-    //при клике на основную кнопку отправляем данные в строковом виде
+    tg.sendData("some string that we need to send");
 });
-
-
-let usercard = document.getElementById("usercard"); //получаем блок usercard 
-
-let profName = document.createElement('p'); //создаем параграф
-profName.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
-//выдем имя, "фамилию", через тире username и код языка
-usercard.appendChild(profName); //добавляем 
-
-let userid = document.createElement('p'); //создаем еще параграф 
-userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-usercard.appendChild(userid); //добавляем
-
-
-//работает только в attachment menu
-// let pic = document.createElement('img'); //создаем img
-// pic.src = tg.initDataUnsafe.user.photo_url; //задаём src 
-// usercard.appendChild(pic); //добавляем элемент в карточку 
